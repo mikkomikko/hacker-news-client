e8ab26202e81365ec5a78629cb9ce42bd53a1c4f
Mikko Suominen <mikko.suominen@houston-inc.com>
2023-09-22 15:14:02 +0300

    Add link to frontpage

--- src/pages/NewsPage/NewsPage.tsx
+++ src/pages/NewsPage/NewsPage.tsx
@@ -1,9 +1,14 @@
 import NewsList from "@/components/NewsList/NewsList";
+import { Link } from "react-router-dom";
 
 export default function NewsPage() {
   return (
     <div className="mt-2xl">
-      <div className="f-step--1 text-primary f-semi-bold">Hacker News</div>
+      <div className="f-step--1 text-primary f-semi-bold">
+        <Link className="link--reset" to="/">
+          Hacker News
+        </Link>
+      </div>
       <h1 className="f-step-5 mt-s mb-l">Top stories</h1>
       <NewsList />
     </div>
